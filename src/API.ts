a6e2e8b46ef1b914cc740873d2a9970577878b81
netajik <kancharapunetaji@gmail.com>
2021-09-18 22:30:33 +0530

    initial commit

---
+++ src/API.ts
@@ -0,0 +1,3 @@
+
+const API = 'https://opentdb.com/api.php?amount=10&category=18&type=multiple';
+export {}
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Utils = () => {
+    
+}
+
+export default Utils;
